9e888c2acddd373b335be01362867c068789d8b9
Ambroise Durand <ambroise1.durand@gmail.com>
2026-05-01 23:05:24 +0200

    Fix wheel and added cursor to main scene

--- wheels-will/Scenes/wheel_colision_horse.gd
+++ wheels-will/Scenes/wheel_colision_horse.gd
@@ -12,5 +12,6 @@ func _process(delta: float) -> void:
 
 
 func _on_area_entered(area: Area2D) -> void:
-	get_parent().lastWinner = id
+	if area.name=="CursorWheel":
+		get_parent().lastWinner = id
 	pass # Replace with function body.
