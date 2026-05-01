bfc4cc207a4d2ad055d98143373cedb803621878
Ambroise Durand <ambroise1.durand@gmail.com>
2026-05-01 17:49:16 +0200

    push color rectangle

--- wheels-will/Scenes/color_rect.gd
+++ wheels-will/Scenes/color_rect.gd
@@ -2,8 +2,11 @@ extends ColorRect
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	color = Color.BROWN
+	body.bet_active = self
+	print(body.bet_active)
 
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	color = Color.REBECCA_PURPLE
-	print(body)
+	body.bet_active = null
+	print(body.bet_active)
