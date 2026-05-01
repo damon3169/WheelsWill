34dda65f43049f2c0759518008ae2daa3a936bce
Paul Eyheramouno <pauljs.contact@gmail.com>
2026-05-01 15:38:24 +0200

    2 player coop

--- wheels-will/Scenes/player_movement.gd
+++ wheels-will/Scenes/player_movement.gd
@@ -1,12 +1,17 @@
 extends CharacterBody2D
 
-
+@export var player_index = 0;
 @export var movement_speed : float = 500
 var character_direction : Vector2
 
 func _physics_process(delta: float):
-	character_direction.x = Input.get_axis("move_left", "move_right")
-	character_direction.y = Input.get_axis("move_up", "move_down")
+	character_direction.x = Input.get_joy_axis(player_index, JOY_AXIS_LEFT_X)
+	if character_direction.x < 0.2 && character_direction.x > -0.2:
+		character_direction.x = 0
+	character_direction.y = Input.get_joy_axis(player_index, JOY_AXIS_LEFT_Y)
+	if character_direction.y < 0.2 && character_direction.y > -0.2:
+		character_direction.y = 0
+	character_direction = character_direction.normalized()
 	
 	#flip
 	if character_direction.x > 0: %sprite.flip_h = false
