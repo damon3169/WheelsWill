02777eeb718f6d258c032db83070c30d71ae44cf
Ambroise Durand <ambroise1.durand@gmail.com>
2026-05-02 23:00:36 +0200

    add reset zone

--- wheels-will/Scenes/buttonNextRound.gd
+++ wheels-will/Scenes/buttonNextRound.gd
@@ -1,9 +1,13 @@
-extends Button
+extends Area2D
 var NumberOfRound = 0
 signal nextRound()
-
+var result = load("res://Scenes/result.tscn")
+var canRelaunch = false
+var players
+var playerIn:Array
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	players = get_tree().get_nodes_in_group("Players")
 	pass # Replace with function body.
 
 
@@ -11,11 +15,40 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func _on_pressed() -> void:
+func onAllPlayerReset() -> void:
 	if NumberOfRound<3:
 		NumberOfRound +=1
 		nextRound.emit()
 		hide()
 	else:
-		var players = get_tree().get_nodes_in_group("Players")
-		get_tree().change_scene_to_file("res://Scenes/ScreenMockup.tscn")
+		var instance = result.instantiate()
+		var resultPlayers: Array
+		get_parent().get_parent().get_parent().add_child(instance)
+		get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+		for i in range(len(players)):
+			resultPlayers.append([players[i].player_index,players[i].score])
+		resultPlayers.sort_custom(func(a, b): return a[1] > b[1])
+		
+
+func sort_ascending(a, b):
+	if a[1] < b[1]:
+		return true
+	return false
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	playerIn.append(body)
+	canRelaunch = true
+	for i in range(len(players)):
+		if playerIn.find(players[i]) ==-1:
+			canRelaunch = false
+			break
+	if canRelaunch:
+		onAllPlayerReset() 
+	pass # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	playerIn.erase(body)
+	pass # Replace with function body.
