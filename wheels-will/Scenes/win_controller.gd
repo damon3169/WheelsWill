7fb516c23d788f5969cf44049693b6b9ec39ca67
Ambroise Durand <ambroise1.durand@gmail.com>
2026-05-02 15:43:58 +0200

    Added win controller

---
+++ wheels-will/Scenes/win_controller.gd
@@ -0,0 +1,30 @@
+extends Node2D
+@export var horseGroup :Array
+@export var horseGroupSorted:Array
+var betGroup: Array
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_finish_line_horse_won(horseHasWon: bool, horseID: int) -> void:
+	horseGroup = get_tree().get_nodes_in_group("HorseGroup")
+	for i in range(len(horseGroup)):
+		horseGroupSorted.append([horseGroup[i].id,horseGroup[i].position.x])
+	horseGroupSorted.sort_custom(func(a, b): return a[1] > b[1])
+	betGroup = get_tree().get_nodes_in_group("BetGroup")
+	for i in range(len(betGroup)):
+		betGroup[i].validate_bet(horseGroupSorted)
+	pass
+
+
+func sort_ascending(a, b):
+	if a[1] < b[1]:
+		return true
+	return false
